d63dde5bf7f7dd6407ce74c6056e29897f500479
yasin-13 <yasin318813@gmail.com>
2024-05-18 13:10:22 +0530

    in

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
 
+
+import { Inter, Poppins } from "next/font/google";
+
+const inter = Poppins({ subsets: ["latin"] ,weight:'400'});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <main className={`${inter.className}`}>
+    <Component {...pageProps} />
+  </main>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons"
+ 
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <h1>IDLI</h1>
-  );
+  return <>
+  <div className="flex flex-col min-h-screen justify-center items-center max-w-4xl mx-auto">
+    <h1 className="text-4xl text-bold max-w-lg text-center">welcome to Admin side Website</h1>
+    <p className="font-medium my-2">admin priveelge/account required to access</p>
+    <Button>
+      <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
+    </Button>
+
+    
+  </div>
+
+  </>
 }
