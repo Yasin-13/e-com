47aa5caa92dc404cb5a599558ff56c9ef352c6c8
yasin-13 <yasin318813@gmail.com>
2024-05-19 14:56:41 +0530

    aaj ka commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import "@/styles/globals.css";
 
 import { SessionProvider } from "next-auth/react"
-
+import Header from "@/components/header";
 import { Inter, Poppins } from "next/font/google";
 
 const inter = Poppins({ subsets: ["latin"] ,weight:'400'});
 
 export default function App({ Component, pageProps : { session, ...pageProps }}) {
   return <main className={`${inter.className}`}>
+
     <SessionProvider session={session}>
+    <Header />
+
+     
       <Component {...pageProps} />
     </SessionProvider>
   </main>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb";
+
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
